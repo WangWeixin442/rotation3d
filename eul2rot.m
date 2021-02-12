b1c45f9a69ea5cbd1f9f5088531712a43ee511d5
weixinwang <weixinwang442@gmail.com>
2021-02-12 12:47:53 -0500

    zyz euler angle convention

--- eul2rot.m
+++ eul2rot.m
@@ -1,7 +1,12 @@
-function [ R ] = eul2rot( e )
+function [ R ] = eul2rot( e, seq )
 % euler angle is represneted as 3-2-1 body sequence (roll, pitch yaw)
 % e is a 3-by-n or n-by-3 matrix
 % R returns a 3-by-3-by-n matrix
+% seq is the sequence of Euler angles: 'zyx' (default) or 'zyz'
+
+if ~exist('seq','var') || isempty(seq)
+    seq = 'zyx';
+end
 
 % check size
 if size(e,2) == 3
@@ -11,15 +16,30 @@ elseif size(e,1) ~= 3
 end
 
 % calculate
-R(1,1,:) = cos(e(2,:)).*cos(e(3,:));
-R(1,2,:) = -cos(e(1,:)).*sin(e(3,:))+sin(e(1,:)).*sin(e(2,:)).*cos(e(3,:));
-R(1,3,:) = sin(e(1,:)).*sin(e(3,:))+cos(e(1,:)).*sin(e(2,:)).*cos(e(3,:));
-R(2,1,:) = cos(e(2,:)).*sin(e(3,:));
-R(2,2,:) = cos(e(1,:)).*cos(e(3,:))+sin(e(1,:)).*sin(e(2,:)).*sin(e(3,:));
-R(2,3,:) = -sin(e(1,:)).*cos(e(3,:))+cos(e(1,:)).*sin(e(2,:)).*sin(e(3,:));
-R(3,1,:) = -sin(e(2,:));
-R(3,2,:) = sin(e(1,:)).*cos(e(2,:));
-R(3,3,:) = cos(e(1,:)).*cos(e(2,:));
+if strcmpi(seq,'zyx')
+    R(1,1,:) = cos(e(2,:)).*cos(e(3,:));
+    R(1,2,:) = -cos(e(1,:)).*sin(e(3,:))+sin(e(1,:)).*sin(e(2,:)).*cos(e(3,:));
+    R(1,3,:) = sin(e(1,:)).*sin(e(3,:))+cos(e(1,:)).*sin(e(2,:)).*cos(e(3,:));
+    R(2,1,:) = cos(e(2,:)).*sin(e(3,:));
+    R(2,2,:) = cos(e(1,:)).*cos(e(3,:))+sin(e(1,:)).*sin(e(2,:)).*sin(e(3,:));
+    R(2,3,:) = -sin(e(1,:)).*cos(e(3,:))+cos(e(1,:)).*sin(e(2,:)).*sin(e(3,:));
+    R(3,1,:) = -sin(e(2,:));
+    R(3,2,:) = sin(e(1,:)).*cos(e(2,:));
+    R(3,3,:) = cos(e(1,:)).*cos(e(2,:));
+elseif strcmpi(seq,'zyz')
+    se = sin(e);
+    ce = cos(e);
+    
+    R(1,1,:) = ce(1,:).*ce(2,:).*ce(3,:)-se(1,:).*se(3,:);
+    R(1,2,:) = -ce(1,:).*ce(2,:).*se(3,:)-se(1,:).*ce(3,:);
+    R(1,3,:) = ce(1,:).*se(2,:);
+    R(2,1,:) = se(1,:).*ce(2,:).*ce(3,:)+ce(1,:).*se(3,:);
+    R(2,2,:) = -se(1,:).*ce(2,:).*se(3,:)+ce(1,:).*ce(3,:);
+    R(2,3,:) = se(1,:).*se(2,:);
+    R(3,1,:) = -se(2,:).*ce(3,:);
+    R(3,2,:) = se(2,:).*se(3,:);
+    R(3,3,:) = ce(2,:);
+end
 
 end
 
--- rot2eul.m
+++ rot2eul.m
@@ -1,14 +1,18 @@
-function [ e ] = rot2eul( R, checko, tran )
+function [ e ] = rot2eul( R, seq, checko, tran )
 % R is a 3-by-3-by-n matrix
 % if tran==false, e returns a 3-by-n matrix
 % if tran==true, e returns a n-by-3 matrix
 % if checko==true (default), check if R is orthogonal with determinant one.
+% seq is the sequence of Euler angles: 'zyx' (default) or 'zyz'
 
 if ~exist('checko','var') || isempty(checko)
     checko = true;
 end
 if ~exist('tran','var') || isempty(tran)
     tran = false;
+end
+if ~exist('seq','var') || isempty(seq)
+    seq = 'zyx';
 end
 
 % check size and orthogonality
@@ -31,8 +35,20 @@ if checko
 end
 
 % calculate
-q = rot2qua(R,checko);
-e = qua2eul(q,checko);
+if strcmpi(seq,'zyx')
+    q = rot2qua(R,checko);
+    e = qua2eul(q,checko);
+elseif strcmpi(seq,'zyz')
+    inds = (abs(R(3,3,:)) == 1);
+    
+    e(1,inds) = atan2(R(2,1,inds),R(2,2,inds)).*sign(R(3,3,inds));
+    e(2,inds) = acos(R(3,3,inds));
+    e(3,inds) = 0;
+    
+    e(1,~inds) = atan2(R(2,3,~inds),R(1,3,~inds));
+    e(2,~inds) = acos(R(3,3,~inds));
+    e(3,~inds) = atan2(R(3,2,~inds),-R(3,1,~inds));
+end
 
 % format result
 if tran
